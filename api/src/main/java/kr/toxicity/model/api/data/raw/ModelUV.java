67692bdbadf97365649ca0948a6e7d7376c0a044
Yang Seong Mo <angryko@naver.com>
2024-12-21 14:04:59 +0900

    Fix UV.

--- api/src/main/java/kr/toxicity/model/api/data/raw/ModelUV.java
+++ api/src/main/java/kr/toxicity/model/api/data/raw/ModelUV.java
@@ -6,11 +6,14 @@ import org.jetbrains.annotations.Nullable;
 
 public record ModelUV(
         @NotNull Float4 uv,
+        float rotation,
         @Nullable String texture
 ) {
     public @NotNull JsonObject toJson(int resolution) {
         var object = new JsonObject();
         object.add("uv", uv.div((float) resolution).toJson());
+        if (rotation != 0) object.addProperty("rotation", rotation);
+        object.addProperty("tintindex", -2);
         object.addProperty("texture", "#" + texture);
         return object;
     }
