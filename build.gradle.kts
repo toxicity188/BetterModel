a69a4bab9d4b83baba6c5421515106f667647776
Yang Seong Mo <angryko@naver.com>
2025-05-06 00:49:15 +0900

    update: 1.5.4

--- build.gradle.kts
+++ build.gradle.kts
@@ -25,7 +25,7 @@ allprojects {
     apply(plugin = "kotlin")
     apply(plugin = "org.jetbrains.dokka")
     group = "kr.toxicity.model"
-    version = "1.5.3" + (buildNumber?.let { "-SNAPSHOT-$it" } ?: "")
+    version = "1.5.4" + (buildNumber?.let { "-SNAPSHOT-$it" } ?: "")
     repositories {
         mavenCentral()
         maven("https://repo.papermc.io/repository/maven-public/")
