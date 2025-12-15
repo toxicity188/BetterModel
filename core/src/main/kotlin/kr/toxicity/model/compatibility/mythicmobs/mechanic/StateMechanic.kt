b3412235182b031a5368f1436d783a7b29c43642
toxicity188 <angryko@naver.com>
2025-12-15 13:35:54 +0900

    feat: per-player animation in MythicMobs state mechanic

--- core/src/main/kotlin/kr/toxicity/model/compatibility/mythicmobs/mechanic/StateMechanic.kt
+++ core/src/main/kotlin/kr/toxicity/model/compatibility/mythicmobs/mechanic/StateMechanic.kt
@@ -6,15 +6,18 @@
  */
 package kr.toxicity.model.compatibility.mythicmobs.mechanic
 
+import io.lumine.mythic.api.adapters.AbstractEntity
 import io.lumine.mythic.api.config.MythicLineConfig
 import io.lumine.mythic.api.skills.INoTargetSkill
+import io.lumine.mythic.api.skills.ITargetedEntitySkill
 import io.lumine.mythic.api.skills.SkillMetadata
 import io.lumine.mythic.api.skills.SkillResult
 import kr.toxicity.model.api.animation.AnimationModifier
 import kr.toxicity.model.api.util.function.FloatConstantSupplier
 import kr.toxicity.model.compatibility.mythicmobs.*
+import org.bukkit.entity.Player
 
-class StateMechanic(mlc: MythicLineConfig) : AbstractSkillMechanic(mlc), INoTargetSkill {
+class StateMechanic(mlc: MythicLineConfig) : AbstractSkillMechanic(mlc), INoTargetSkill, ITargetedEntitySkill {
 
     private val model = mlc.modelPlaceholder
     private val state = mlc.toPlaceholderString(arrayOf("state", "s"))
@@ -24,14 +27,23 @@ class StateMechanic(mlc: MythicLineConfig) : AbstractSkillMechanic(mlc), INoTarg
     private val remove = mlc.toPlaceholderBoolean(arrayOf("remove", "r"))
 
     override fun cast(p0: SkillMetadata): SkillResult {
-        val args = p0.toPlaceholderArgs()
-        return p0.toTracker(model(args))?.let {
+        return cast(null, p0)
+    }
+
+    override fun castAtEntity(p0: SkillMetadata, p1: AbstractEntity): SkillResult {
+        return cast(p1.bukkitEntity as? Player, p0)
+    }
+
+    private fun cast(player: Player?, meta: SkillMetadata): SkillResult {
+        val args = meta.toPlaceholderArgs()
+        return meta.toTracker(model(args))?.let {
             val s = state(args) ?: return SkillResult.CONDITION_FAILED
-            if (remove(args)) it.stopAnimation(s) else it.animate(s, AnimationModifier(
-                li(args),
-                lo(args),
-                sp(args)?.let(FloatConstantSupplier::of)
-            ))
+            if (remove(args)) it.stopAnimation(s) else it.animate(s, AnimationModifier.builder()
+                .start(li(args))
+                .end(lo(args))
+                .speed(sp(args)?.let(FloatConstantSupplier::of))
+                .player(player)
+                .build())
             SkillResult.SUCCESS
         } ?: SkillResult.CONDITION_FAILED
     }
