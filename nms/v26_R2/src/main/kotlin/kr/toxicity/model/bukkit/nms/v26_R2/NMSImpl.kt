af2dda9c3d57248ff7183d18fc199695bfa74038
toxicity188 <angryko@naver.com>
2026-06-13 09:35:30 +0900

    feat: initial 26.2 nms

---
+++ nms/v26_R2/src/main/kotlin/kr/toxicity/model/bukkit/nms/v26_R2/NMSImpl.kt
@@ -0,0 +1,385 @@
+/*
+ * This source file is part of BetterModel.
+ * Copyright (c) 2026 toxicity188
+ * Licensed under the MIT License.
+ * See LICENSE.md file for full license text.
+ */
+
+package kr.toxicity.model.bukkit.nms.v26_R2
+
+import ca.spottedleaf.moonrise.patches.chunk_system.level.entity.EntityLookup
+import com.mojang.authlib.GameProfile
+import io.netty.channel.ChannelDuplexHandler
+import io.netty.channel.ChannelHandlerContext
+import io.netty.channel.ChannelPromise
+import kr.toxicity.model.api.BetterModel
+import kr.toxicity.model.api.bone.RenderedBone
+import kr.toxicity.model.api.bukkit.BetterModelBukkit
+import kr.toxicity.model.api.bukkit.entity.BaseBukkitEntity
+import kr.toxicity.model.api.data.blueprint.ModelBoundingBox
+import kr.toxicity.model.api.entity.BaseEntity
+import kr.toxicity.model.api.entity.BasePlayer
+import kr.toxicity.model.api.mount.MountController
+import kr.toxicity.model.api.nms.*
+import kr.toxicity.model.api.platform.PlatformEntity
+import kr.toxicity.model.api.platform.PlatformItemStack
+import kr.toxicity.model.api.platform.PlatformLocation
+import kr.toxicity.model.api.platform.PlatformPlayer
+import kr.toxicity.model.api.player.PlayerSkinParts
+import kr.toxicity.model.api.profile.ModelProfile
+import kr.toxicity.model.api.tracker.EntityTrackerRegistry
+import kr.toxicity.model.api.tracker.TrackerUpdateAction
+import kr.toxicity.model.api.util.TransformedItemStack
+import net.kyori.adventure.key.Keyed
+import net.minecraft.core.component.DataComponents
+import net.minecraft.network.Connection
+import net.minecraft.network.protocol.Packet
+import net.minecraft.network.protocol.game.*
+import net.minecraft.network.syncher.EntityDataSerializers
+import net.minecraft.network.syncher.SynchedEntityData
+import net.minecraft.resources.Identifier
+import net.minecraft.server.MinecraftServer
+import net.minecraft.server.level.ServerLevel
+import net.minecraft.server.network.ServerCommonPacketListenerImpl
+import net.minecraft.util.ARGB
+import net.minecraft.world.entity.Display
+import net.minecraft.world.entity.Display.ItemDisplay
+import net.minecraft.world.entity.Entity
+import net.minecraft.world.entity.EntityTypes
+import net.minecraft.world.entity.LivingEntity
+import net.minecraft.world.item.ItemDisplayContext
+import net.minecraft.world.item.Items
+import net.minecraft.world.item.component.CustomModelData
+import net.minecraft.world.item.component.DyedItemColor
+import net.minecraft.world.level.entity.LevelEntityGetter
+import net.minecraft.world.level.entity.LevelEntityGetterAdapter
+import net.minecraft.world.level.entity.PersistentEntitySectionManager
+import org.bukkit.craftbukkit.CraftWorld
+import org.bukkit.craftbukkit.entity.CraftEntity
+import org.bukkit.craftbukkit.entity.CraftPlayer
+import org.joml.Vector3d
+import java.util.*
+import java.util.function.Consumer
+import java.util.function.IntConsumer
+
+class NMSImpl : NMS {
+
+    companion object {
+        private const val INJECT_NAME = "bettermodel_channel_handler"
+
+        //Spigot
+        private val getGameProfile: (net.minecraft.world.entity.player.Player) -> GameProfile = createAdaptedFieldGetter { it.gameProfile }
+        private val getConnection: (ServerCommonPacketListenerImpl) -> Connection = createAdaptedFieldGetter { it.connection }
+        private val spigotChunkAccess = ServerLevel::class.java.fields.firstOrNull {
+            it.type == PersistentEntitySectionManager::class.java
+        }?.apply {
+            isAccessible = true
+        }
+        @Suppress("UNCHECKED_CAST")
+        private val ServerLevel.levelGetter
+            get(): LevelEntityGetter<Entity> {
+                return if (BetterModelBukkit.IS_PAPER) {
+                    `moonrise$getEntityLookup`()
+                } else {
+                    spigotChunkAccess?.get(this)?.let {
+                        (it as PersistentEntitySectionManager<*>).entityGetter as LevelEntityGetter<Entity>
+                    } ?: throw RuntimeException("LevelEntityGetter")
+                }
+            }
+        private val getEntityById: (LevelEntityGetter<Entity>, Int) -> Entity? = if (BetterModelBukkit.IS_PAPER) { g, i ->
+            (g as EntityLookup)[i]
+        } else LevelEntityGetterAdapter::class.java.declaredFields.first {
+            net.minecraft.world.level.entity.EntityLookup::class.java.isAssignableFrom(it.type)
+        }.let {
+            it.isAccessible = true
+            { e, i ->
+                (it[e] as net.minecraft.world.level.entity.EntityLookup<*>).getEntity(i) as? Entity
+            }
+        }
+        private fun Int.toEntity(level: ServerLevel) = getEntityById(level.levelGetter, this)
+        //Spigot
+        private val hitBoxData by lazy {
+            ItemDisplay(EntityTypes.ITEM_DISPLAY, MinecraftServer.getServer().overworld()).run {
+                entityData[Display.DATA_POS_ROT_INTERPOLATION_DURATION_ID] = 3
+                entityData.nonDefaultValues!!
+            }
+        }
+    }
+
+    override fun hide(channel: PlayerChannelHandler, registry: EntityTrackerRegistry) {
+        val target = registry.entity().handle() as? Entity ?: return
+        val list = bundlerOf()
+        target.entityData.pack(
+            valueFilter = { it.id == SHARED_FLAG }
+        )?.let {
+            list += ClientboundSetEntityDataPacket(target.id, it).toRegistryDataPacket(channel.uuid(), registry)
+        }
+        if (target is LivingEntity) {
+            val packet = if (registry.hideOption(channel.uuid()).equipment) target.toEmptyEquipmentPacket() else target.toEquipmentPacket()
+            packet?.let { list += it }
+        }
+        list.send(channel.player())
+    }
+
+    private fun ClientboundSetEntityDataPacket.toRegistryDataPacket(uuid: UUID, registry: EntityTrackerRegistry) = ClientboundSetEntityDataPacket(id, packedItems().map {
+        if (it.id == SHARED_FLAG) SynchedEntityData.DataValue(
+            it.id,
+            EntityDataSerializers.BYTE,
+            registry.entityFlag(uuid, it.value() as Byte)
+        ) else it
+    })
+
+    inner class PlayerChannelHandlerImpl(
+        private val player: CraftPlayer
+    ) : PlayerChannelHandler, ChannelDuplexHandler() {
+        private val connection = player.handle.connection
+        private val uuid = player.uniqueId
+        private val base = adapt(player.wrap())
+
+        init {
+            val pipeline = getConnection(connection).channel.pipeline()
+            pipeline.addBefore(pipeline.first { it.value is Connection }.key, INJECT_NAME, this)
+        }
+
+        override fun close() {
+            val channel = getConnection(connection).channel
+            channel.eventLoop().submit {
+                channel.pipeline().remove(INJECT_NAME)
+            }
+        }
+
+        override fun base(): BasePlayer = base
+        override fun isModEnabled(): Boolean = (if (BetterModelBukkit.IS_PAPER) player.channels() else player.listeningPluginChannels).contains(ModAnimationBundlerImpl.KEY)
+
+        private val playerModel get() = connection.player.id.toRegistry()
+
+        private fun Int.toPlayerEntity() = toEntity(connection.player.level())
+        private fun Entity.toRegistry() = BetterModel.registryOrNull(uuid)
+        private inline fun Int.toRegistry(
+            ifHitBox: (Entity) -> Unit = {}
+        ) = (EntityTrackerRegistry.registry(this) ?: toPlayerEntity()?.let {
+            if (it is HitBox) ifHitBox(it)
+            it.toRegistry()
+        })?.takeIf {
+            it.isSpawned(player.uniqueId)
+        }
+
+        override fun sendEntityData(registry: EntityTrackerRegistry) {
+            val handle = registry.entity().handle() as? Entity ?: return
+            val list = bundlerOf(
+                ClientboundSetPassengersPacket(handle)
+            )
+            handle.entityData.pack(
+                valueFilter = { it.id == SHARED_FLAG }
+            )?.let {
+                list += ClientboundSetEntityDataPacket(handle.id, it)
+            }
+            if (handle is LivingEntity) handle.toEquipmentPacket()?.let {
+                list += it
+            }
+            list.send(player.wrap())
+        }
+
+        private fun <T : ClientGamePacketListener> Packet<in T>.handle(): Packet<in T>? {
+            when (this) {
+                is ClientboundBundlePacket -> return if (subPackets() is Keyed) this else ClientboundBundlePacket(subPackets().mapNotNull {
+                    it.handle()
+                })
+                is ClientboundAddEntityPacket -> {
+                    val entity = id.toPlayerEntity() ?: return this
+                    if (entity is HitBox) return entity.toFakeAddPacket()
+                    val wrap = entity.bukkitEntity.wrap()
+                    BetterModel.registry(wrap).ifPresent {
+                        wrap.taskLater(1) {
+                            it.spawn(player.wrap())
+                        }
+                    }
+                }
+                is ClientboundRemoveEntitiesPacket -> {
+                    entityIds
+                        .asSequence()
+                        .mapNotNull map@ {
+                            it.toRegistry {
+                                return@map null
+                            }
+                        }
+                        .forEach {
+                            it.remove()
+                        }
+                }
+                is ClientboundSetPassengersPacket -> {
+                    vehicle.toRegistry()?.let {
+                        return it.mountPacket(it.entity().handle() as? Entity ?: return this, array = passengers)
+                    }
+                }
+                is ClientboundUpdateAttributesPacket if entityId.toPlayerEntity() is HitBox -> return null
+                is ClientboundSetEntityDataPacket -> id.toRegistry {
+                    return ClientboundSetEntityDataPacket(id, hitBoxData)
+                }?.let { registry ->
+                    return toRegistryDataPacket(uuid, registry)
+                }
+                is ClientboundSetEquipmentPacket -> entity.toRegistry {
+                    return null
+                }?.let {
+                    if (it.hideOption(uuid).equipment()) (it.entity().handle() as? LivingEntity)?.toEmptyEquipmentPacket()?.let { packet ->
+                        return packet
+                    }
+                }
+                is ClientboundRespawnPacket -> playerModel?.let {
+                    bundlerOf(it.mountPacket(connection.player)).send(player.wrap())
+                }
+                is ClientboundContainerSetSlotPacket if isEquipment(connection.player) && playerModel?.hideOption(uuid)?.equipment() == true -> {
+                    return ClientboundContainerSetSlotPacket(containerId, stateId, slot, EMPTY_ITEM)
+                }
+                is ClientboundContainerSetContentPacket if containerId == 0 && playerModel?.hideOption(uuid)?.equipment() == true -> {
+                    return ClientboundContainerSetContentPacket(
+                        containerId,
+                        stateId,
+                        items.apply {
+                            PLAYER_EQUIPMENT_SLOT.forEach(IntConsumer { set(it, EMPTY_ITEM) })
+                            set(connection.player.hotbarSlot, EMPTY_ITEM)
+                        },
+                        carriedItem
+                    )
+                }
+            }
+            return this
+        }
+
+        override fun write(ctx: ChannelHandlerContext, msg: Any, promise: ChannelPromise) {
+            super.write(ctx, if (msg is Packet<*>) msg.handle() ?: return else msg, promise)
+        }
+
+        override fun channelRead(ctx: ChannelHandlerContext, msg: Any) {
+            fun EntityTrackerRegistry.updatePlayerLimb() = BetterModel.platform().scheduler().asyncTaskLater(1) {
+                if (isClosed) return@asyncTaskLater
+                player.handle.containerMenu.sendAllDataToRemote()
+                trackers().forEach { tracker ->
+                    tracker.update(TrackerUpdateAction.itemMapping()) { bone ->
+                        !bone.itemMapper.fixed()
+                    }
+                }
+            }
+            when (msg) {
+                is ServerboundSetCarriedItemPacket -> {
+                    playerModel?.let { registry ->
+                        if (!registry.hideOption(uuid).equipment()) return super.channelRead(ctx, msg)
+                        if (CONFIG.cancelPlayerModelInventory()) {
+                            connection.send(ClientboundSetHeldSlotPacket(player.inventory.heldItemSlot))
+                            return
+                        }
+                        registry.updatePlayerLimb()
+                    }
+                }
+                is ServerboundPlayerActionPacket -> {
+                    playerModel?.let { registry ->
+                        if (!registry.hideOption(uuid).equipment()) return super.channelRead(ctx, msg)
+                        if (CONFIG.cancelPlayerModelInventory()) return
+                        registry.updatePlayerLimb()
+                    }
+                }
+            }
+            super.channelRead(ctx, msg)
+        }
+
+        private fun EntityTrackerRegistry.remove() {
+            remove(player.wrap())
+        }
+    }
+
+    override fun mount(registry: EntityTrackerRegistry, bundler: PacketBundler) {
+        val entity = registry.entity().handle()
+        if (entity is Entity) bundler += registry.mountPacket(entity)
+    }
+
+    private fun EntityTrackerRegistry.mountPacket(entity: Entity, array: IntArray = entity.passengers.filter {
+        EntityTrackerRegistry.registry(it.uuid) == null
+    }.map {
+        it.id
+    }.toIntArray()): ClientboundSetPassengersPacket {
+        return useByteBuf { buffer ->
+            buffer.writeVarInt(entity.id)
+            buffer.writeVarIntArray(displays()
+                .mapToInt {
+                    (it as ModelDisplayImpl).display.id
+                }.toArray() + array)
+            ClientboundSetPassengersPacket.STREAM_CODEC.decode(buffer)
+        }
+    }
+
+    override fun inject(player: PlatformPlayer): PlayerChannelHandlerImpl = PlayerChannelHandlerImpl(player.unwarp() as CraftPlayer)
+
+    override fun createBundler(initialCapacity: Int): PacketBundler = bundlerOf(initialCapacity)
+    override fun createParallelBundler(threshold: Int): PacketBundler = parallelBundlerOf(threshold)
+    override fun createModAnimationBuilder(initialCapacity: Int): ModAnimationBundler = ModAnimationBundlerImpl(initialCapacity)
+
+    override fun create(location: PlatformLocation, yOffset: Double, initialConsumer: Consumer<ModelDisplay>): ModelDisplay = ModelDisplayImpl(
+        Vector3d(location.x(), location.y(), location.z()),
+        ItemDisplay(EntityTypes.ITEM_DISPLAY, (location.world().unwarp() as CraftWorld).handle).apply {
+            entityData[Display.DATA_POS_ROT_INTERPOLATION_DURATION_ID] = 3
+            billboardConstraints = Display.BillboardConstraints.FIXED
+            valid = true
+            yRot = location.yaw()
+            itemTransform = ItemDisplayContext.FIXED
+        },
+        yOffset
+    ).apply {
+        initialConsumer.accept(this)
+        display.entityData.packDirty()
+    }
+
+    override fun createNametag(bone: RenderedBone): ModelNametag = ModelNametagImpl(bone)
+
+    override fun tint(itemStack: PlatformItemStack, rgb: Int): PlatformItemStack {
+        return itemStack.unwarp().asVanilla().apply {
+            set(DataComponents.DYED_COLOR, DyedItemColor(rgb))
+            set(DataComponents.CUSTOM_MODEL_DATA, get(DataComponents.CUSTOM_MODEL_DATA)?.let {
+                CustomModelData(it.floats, it.flags, it.strings, it.colors
+                    .run {
+                        if (rgb == 0xFFFFFF) this else map { color ->
+                            ARGB.multiply(color, rgb) and 0xFFFFFF
+                        }
+                    }
+                    .ifEmpty { listOf(rgb) })
+            })
+        }.asBukkit().wrap()
+    }
+
+    override fun createHitBox(entity: BaseEntity, bone: RenderedBone, boundingBox: ModelBoundingBox, mountController: MountController, listener: HitBoxListener): HitBox? {
+        val handle = entity.handle() as? Entity ?: return null
+        return HitBoxImpl(
+            boundingBox.center(),
+            bone,
+            listener,
+            handle,
+            mountController
+        ).craftEntity
+    }
+    override fun version(): NMSVersion = NMSVersion.V26_R1
+
+    override fun adapt(entity: PlatformEntity): BaseBukkitEntity {
+        val craft = entity.unwarp() as CraftEntity
+        return BaseEntityImpl(craft)
+    }
+
+    override fun adapt(player: PlatformPlayer): BasePlayer {
+        val craft = player.unwarp() as CraftPlayer
+        return BasePlayerImpl(
+            craft,
+            dirtyChecked({ getGameProfile(craft.handle) }, { ModelGameProfile(it) }),
+            dirtyChecked({ craft.handle.toCustomisation() }, { PlayerSkinParts(it) })
+        )
+    }
+
+    override fun profile(player: PlatformPlayer): ModelProfile = ModelGameProfile(getGameProfile((player.unwarp() as CraftPlayer).handle))
+
+    override fun createSkinItem(model: String, floats: List<Float>, flags: List<Boolean>, strings: List<String>, colors: List<Int>): TransformedItemStack {
+        return VanillaItemStack(Items.PLAYER_HEAD).run {
+            set(DataComponents.CUSTOM_MODEL_DATA, CustomModelData(floats, flags, strings, colors))
+            set(DataComponents.ITEM_MODEL, Identifier.parse(model))
+            TransformedItemStack.of(asBukkit().wrap())
+        }
+    }
+
+    override fun isProxyOnlineMode(): Boolean = ONLINE_MODE
+}
