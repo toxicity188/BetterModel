a291642c6031dd750a4a08e578cedf2df7423155
toxicity188 <angryko@naver.com>
2026-06-18 20:17:43 +0900

    chore: update purpur-api to v26.2.build.+

--- purpur/build.gradle.kts
+++ purpur/build.gradle.kts
@@ -5,6 +5,5 @@ plugins {
 dependencies {
     compileOnly(project(":bettermodel-api"))
     compileOnly(project(":bettermodel-api:bettermodel-bukkit-api"))
-    //TODO compileOnly("org.purpurmc.purpur:purpur-api:${property("minecraft_version")}.build.+")
-    compileOnly("org.purpurmc.purpur:purpur-api:26.1.2.build.+")
+    compileOnly("org.purpurmc.purpur:purpur-api:${property("minecraft_version")}.build.+")
 }
